88f2e1c3e0bcc4a97f0795bd6a0f740f4aa7a459
neiljmcl <neiljmcl@gmail.com>
2016-04-10 21:31:07 +0100

    Attempt to minimally reproduce the bug with the ui-router link to a nested path.

--- app/app.js
+++ app/app.js
@@ -1,12 +1,62 @@
 'use strict';
+angular.module('vdsAdmin', [
+  'ui.router'
+]).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.vegetables',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/vegetables'});
-}]);
+  console.log("Setting vdsAdmin shit up: ");
+
+  $urlRouterProvider
+    .when('', '/grid')
+    .otherwise('notfound');
+
+  $stateProvider
+    .state('taxonomy', {
+      url: '/taxonomy/{path:^.*}',
+      templateUrl: 'taxonomy/taxonomy.html',
+        controller: 'TaxonomyController',
+      onStart(function() {})
+    }).state('grid', {
+      url: '/grid',
+      templateUrl: 'grid.html',
+      controller: 'GridController'
+    }).state('notfound', {
+      url: '/not_found',
+      templateUrl: 'not_found.html'
+    });
+}])
+  .controller('TaxonomyController', ['$scope', '$stateParams', '$log', 'TaxonomyService',
+    function($scope, $stateParams, $log, taxonomyService) {
+      $log.info("This is your taxonomy controller with path ", $stateParams.path, $stateParams.path === 'foo/bar');
+      $scope.title = "Welcome to the taxonomy page";
+      $scope.parent = "Make";
+      $scope.child = "Model";
+      $scope.path = $stateParams.path;
+      $scope.taxonomyPaths = _.map(taxonomyService.children(), function(child) {
+        return taxonomyService.path(child.taxonomyId);
+      });
+      $scope.get = function() {
+        taxonomyService.get($scope.path);
+      }
+    }]).controller('GridController', ['$scope', '$log', function($scope, $log) {
+    $log.info("This is your grid controller");
+    $scope.motd = "This is your grid";
+
+  }])
+  .service('TaxonomyService', ['$log', function($log) {
+    var service = {
+      children: function() {
+        return [
+          {name: 'foo', taxonomyId:'foo'},
+          {name: 'bar', taxonomyId:'foo/bar'},
+          {name: 'baa', taxonomyId:'foo/bar/baa'}
+        ];
+      },
+      path: function(taxonomyId) {
+        return {path: taxonomyId};
+      },
+      get: function(path) {
+        $log.info("Get: ", path, path.split("/"));
+      }
+    };
+    return service;
+  }])
